fee7e6f22c92c5f7ece2635fb1416b799b4721cd
lcbarnett <l.c.barnett@sussex.ac.uk>
2024-07-12 17:26:28 +0100

    Changed jotest_var.m to jotest_autocov.m

--- extra/jotest_autocov.m
+++ extra/jotest_autocov.m
@@ -1,18 +1,22 @@
-function stats = jotest_var(A,V,normevs)
+function stats = jotest_autocov(G,normevs)
 
-% Calculate eigenvalues for VECM likelihood function from VAR parameters.
+% Calculate eigenvalues for VECM likelihood function from autocovariance sequence
+% of a stable process (so not actually integrated!)
+%
+% If G is derived from a VAR model, the number p of autocovariance lags should be
+% equal to the VAR model order; i.e., size(G) = [n,n,p+1].
 
-if nargin < 3 || isempty(normevs), normevs = true; end % normalise eigenvectors?
+if nargin < 2 || isempty(normevs), normevs = true; end % normalise eigenvectors?
 
-[n,~,p] = size(A);
+[n,~,pp1] = size(G);
+p = pp1-1;
+pm1 = p-1;
+np1 = pm1*n;
 
-G = var_to_autocov(A,V,-p);    % n x n x (p+1) : G_0,..., G_p (-p means return exactly p lags)
-
-P = G(:,:,1:p) - G(:,:,2:p+1); % n x n x p     : P_0,..., P_{p-1}
-
-Q = zeros(n,n,p);              % n x n x p     : Q_0,..., Q_{p-1}
-Q(:,:,1)   = P(:,:,1) + P(:,:,1)'; % Q_0 = P_0 + P_0'
-Q(:,:,2:p) = P(:,:,2:p) - P(:,:,1:p-1);
+P = G(:,:,1:p) - G(:,:,2:pp1);          % n x n x p : P_0,..., P_{p-1}
+Q = zeros(n,n,p);                       % n x n x p : Q_0,..., Q_{p-1}
+Q(:,:,1)   = P(:,:,1)   + P(:,:,1)';    % Q_0 = P_0 + P_0'
+Q(:,:,2:p) = P(:,:,2:p) - P(:,:,1:pm1);
 
 % Set up regressions
 %
@@ -21,13 +25,12 @@ Q(:,:,2:p) = P(:,:,2:p) - P(:,:,1:p-1);
 % which uses an algorithm described in Hirotugu Akaike, "Block Toeplitz Matrix Inversion",
 % SIAM J. Appl. Math. 24(2):234-241, 1973.
 
-QBT = sbtoeplitz(Q(:,:,1:p-1)); % symmetric block-Toeplitz of Q_0, ..., Q_{p-2}
+QBT = sbtoeplitz(Q(:,:,1:pm1)); % symmetric block-Toeplitz of Q_0, ..., Q_{p-2}
 
 QBTCHOL = chol(QBT); % upper Cholesky factor, so QBTCHOL'*QBTCHOL = QBT
 
-np1 = (p-1)*n;
 J = reshape(Q(:,:,2:p  ),n,np1)/QBTCHOL;
-K = reshape(P(:,:,1:p-1),n,np1)/QBTCHOL;
+K = reshape(P(:,:,1:pm1),n,np1)/QBTCHOL;
 
 % (Cross-)covariance matrices
 
