972194039fdc527cce538009be6ef452320a494a
lcbarnett <l.c.barnett@sussex.ac.uk>
2024-08-23 12:11:39 +0100

    Added window type parameter to extra/cpsd_me

--- extra/cpsd_me.m
+++ extra/cpsd_me.m
@@ -1,14 +1,15 @@
-function [S,f,window,overlap,nfft] = cpsd_me(X,fs,window,overlap,nfft,autospec,verb)
+function [S,f,nwindow,noverlap,nfft] = cpsd_me(X,fs,nwindow,noverlap,wintype,nfft,autospec,verb)
 
 % Welch method CPSD or PSD for multi-epoched data
 
 [nchans,nobs,nepochs] = size(X);
 
-if nargin < 3 || isempty(window),   window   = round(nobs/2);    end
-if nargin < 4 || isempty(overlap),  overlap  = round(window/2);  end
-if nargin < 5 || isempty(nfft),     nfft     = 2^nextpow2(nobs); else, assert(mod(nfft,2)==0,'nfft must be even!'); end
-if nargin < 6 || isempty(autospec), autospec = false;            end
-if nargin < 7 || isempty(verb),     verb     = true;             end
+if nargin < 3 || isempty(nwindow),   nwindow  = round(nobs/2);    end
+if nargin < 4 || isempty(noverlap),  noverlap = round(nwindow/2); end
+if nargin < 5 || isempty(wintype),   window   = nwindow;          else, window = wintype(nwindow); end
+if nargin < 6 || isempty(nfft),      nfft     = 2^nextpow2(nobs); else, assert(mod(nfft,2)==0,'nfft must be even!'); end
+if nargin < 7 || isempty(autospec),  autospec = false;            end
+if nargin < 8 || isempty(verb),      verb     = true;             end
 
 f = (((0:nfft/2)')/nfft)*fs; % frequencies on [0,fs/2]
 
@@ -23,7 +24,7 @@ X = permute(X,[2 1 3]);
 
 if autospec % NOTE: Autospectra (PSDs) matrix S is frequencies x channels
 	for e = 1:nepochs
-		S = S + pwelch(X(:,:,e),window,overlap,nfft,fs);
+		S = S + pwelch(X(:,:,e),window,noverlap,nfft,fs);
 	end
 	S = S/nepochs;
 	S(1  ,:) = 2*S(1,  :);
@@ -33,7 +34,7 @@ else        % NOTE: Cross-spectra (CPSDs) array S is channels x channels x frequ
 	for e = 1:nepochs
 		if verb, fprintf('\tepoch %3d of %3d\n',e,nepochs); end
 		for i = 1:nchans
-			S(:,:,i) = S(:,:,i) + cpsd(X(:,i,e),X(:,:,e),window,overlap,nfft,fs);
+			S(:,:,i) = S(:,:,i) + cpsd(X(:,i,e),X(:,:,e),window,noverlap,nfft,fs);
 		end
 	end
 	S = permute(S/nepochs,[3 2 1]);
