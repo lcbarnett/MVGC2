b8c2480161b1a17801293c031db8625b188326a0
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2024-08-30 11:34:35 +0100

    Tweaked decorrelation length code

--- utils/var_decorrlen.m
+++ utils/var_decorrlen.m
@@ -8,9 +8,11 @@ if nargin < 3 || isempty(maxlags) % calculate using VAR spectral norm
 		m = Inf; % unstable!
 	else
 		if nargin < 5 || isempty(tol), tol = eps; end
-		m = ceil((-log(eps)+log(max(abs(eig(V)))))/(-log(rho)));
+		m = ceil((log(eps)-log(norm(V)))/log(rho));
+		p = size(A,3);
+		if m < p, m = p; end
 	end
-else                              % calculate using VAR autocovariance sequence
+else                              % calculate using VAR autocovariance sequence (more reliable)
 	assert(isscalar(maxlags) && isint(maxlags) && maxlags > 0,'maximum lags parameter must be a positive integer');
 	if nargin < 5, tol = []; end
 	[~,m] = var_to_autocov(A,V,maxlags,tol);
